ef1cbe52ede04a286eab2f2c1a74feef983c7348
saulocaceresg <scaceresg@unsa.edu.pe>
2025-12-22 17:39:22 -0500

    agregado expreiones regulares y creación del servidor al final

--- PYTHON/Python (extra) - Calculadora con CGI-Python/cgi-bin/script.py
+++ PYTHON/Python (extra) - Calculadora con CGI-Python/cgi-bin/script.py
@@ -18,11 +18,45 @@ class MiServidor(BaseHTTPRequestHandler):
         operacion = query.get("operacion", [""])[0]
 
         html = leer_template(TEMPLATE_PATH)
+        resultado_html = ""
 
-        regex = r"^\d+ [+-*/%] \d+"
-        html = re.search(regex, html)
+        regex = r"^\s(\d+)\s*([+-*/%])\s*(\d+)\s*$"
+        match = re.match(regex, operacion)
+
+        if match:
+            a = int(match.group(1))
+            operador = match.group(2)
+            b = int(match.group(3))
+
+            try:
+                resultado = 0
+                if operador == "+":
+                    resultado = a + b
+                elif operador == "-":
+                    resultado = a - b
+                elif operador == "*":
+                    resultado = a * b
+                elif operador == "/":
+                    resultado = a / b
+                elif operador == "%":
+                    resultado = a % b
+
+                resultado_html = f"<h2>Resultado: {resultado}</h2>"
+
+            except ZeroDivisionError:
+                resultado_html = "<h2>Error: división entre cero</h2>"
+        else:
+            if operacion:
+                resultado_html = "<h2>Expresión inválida</h2>"
+        
+        html = html.replace("<!--RRESULTADO-->", resultado_html)
 
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
         self.wfile.write(html.encode("utf-8"))
+
+if __name__ == "__main__":
+    servidor = HTTPServer(("localhost", 8000), MiServidor)
+    print("Servdor corriendo en http://localhost:8000")
+    servidor.serve_forever()
