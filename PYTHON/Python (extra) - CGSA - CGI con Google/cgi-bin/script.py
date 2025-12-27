4cdbc16e8bc8518b3f7a5432efce2c90eee59865
saulocaceresg <scaceresg@unsa.edu.pe>
2025-12-27 18:08:02 -0500

    arreglando la conexión con google

--- PYTHON/Python (extra) - CGSA - CGI con Google/cgi-bin/script.py
+++ PYTHON/Python (extra) - CGSA - CGI con Google/cgi-bin/script.py
@@ -17,19 +17,42 @@ busqueda = form.getvalue("search")
 # Si no se enviaron datos → mostrar formulario
 if not busqueda:
     print("""
-    <h1>Búsqueda Simple</h1>
-    <br>
-    <form id="frm-simple" action="scripts/script.py" method="post">
-        <input type="text" name="search" id="input-search-simple">
-        <br><br>
-        <button type="submit" id="btn-simple">Búsqueda con Google</button>
-    </form>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Búsquedas con Google | Búsqueda Simple</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Búsqueda Simple</a></li>
+                    <li><a href="searchimages.html">Búsqueda de Imágenes</a></li>
+                    <li><a href="advancedsearch.html">Búsqueda Avanzada</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main id="simplesearch">
+            <h1>Búsqueda Simple</h1>
+            <br>
+            <form id="frm-simple" action="/cgi-bin/script.py" method="get">
+                <input type="text" name="search" id="input-search-simple" value="hola">
+                <br><br>
+                <button type="submit" id="btn-simple">Búsqueda con Google</button>
+            </form>
+        </main>
+    </body>
+    </html>
     """)
     exit()
 
 query = urllib.parse.quote_plus(busqueda)
 google_url = f"https://www.google.com/search?q={query}"
 
-print(f"<p>Buscando {busqueda}</p>")
 print("Status: 302 Found")
 print(f"Location: {google_url}")
+print()
